b07d341282f550e8a02ed17c9088914f59293099
Nikki <Nikkisali9@gmail.com>
2019-11-02 11:31:17 -0600

    notifications

---
+++ Bookcase/BookCase/BookCase/UIViewExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewExtension.swift
+//  BookCase
+//
+//  Created by Marzieh on 2019-11-02.
+//  Copyright © 2019 Myph. All rights reserved.
+//
+
+import UIKit
+
+extension UIView { //finding which view is the first responder
+    var firstResponder:UIView? {
+        if self.isFirstResponder {
+            return self
+        }
+        for view in self.subviews {
+            if let firstResponder = view.firstResponder {
+                return firstResponder
+            }
+        }
+        return nil
+    }
+}
+
+extension UIViewController { //for conveneince
+    var firstResponder:UIView? {
+        return view.firstResponder
+    }
+}
--- Bookcase/BookCase/BookViewController.swift
+++ Bookcase/BookCase/BookViewController.swift
@@ -18,6 +18,37 @@ class BookViewController: UIViewController {
 
         // you can put an infoButton and make the isbn isHidden here
     }
+    
+    @objc func keyboardFrameChanges(notification:Notification) {
+         //@objc is because of #selector
+        
+        guard let userInfo = notification.userInfo else { return }
+        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        var offset:CGFloat = 0
+        if let firstResponder = firstResponder {
+            let frFrame = self.view.convert(firstResponder.frame, from: firstResponder) // because it was in stackview, we have to convert it to the root view coordinates
+            let frMaxY = frFrame.maxY // the max y coordinate of the first responder frame
+            if frMaxY > keyboardFrame.origin.y {
+                offset = frMaxY - keyboardFrame.origin.y
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardFrameChanges), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    } // for object:nil, you can choose to recieve notification from a specific publisher
+    
+    override func viewDidDisappear(_ animated: Bool) { //when scene is no longer active, we want the viewController to stop recieving notifications
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         view.endEditing(true) //goes on a hunt on its subview hierarchy to find the first responder, then requests it to resign
